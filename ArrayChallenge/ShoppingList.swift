41db526c5a21b4e5a23a8161118b37e6a0e435cb
Serg T <sergtsaeb@gmail.com>
2016-09-28 22:35:17 -0700

    function works, fails local testing

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -9,14 +9,35 @@
 
 class ShoppingList {
     
+    var items:[String] = ["Bananas", "Apples", "Eggs", "Rolls"]
+    var amounts:[String] = ["6", "4", "12", "4"]
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
+        let shoppingList = ShoppingList()
+        var amtItems:[String] = []
+        var completeList:[String] = []
         
-       
-        // Implement this function
+        
+        for (item, amount) in zip(items, amounts)
+        {
+            amtItems.append("\(item)(\(amount))")
+        }
+        
+        for (index, stuff) in amtItems.enumerated()
+        {
+            completeList.append("\(index + 1). \(stuff)")
+        }
         
         
-        
+        let generatedList = shoppingList.createShoppingList(withItems: items, amountOfEachItem: amounts)
+        return generatedList
     }
     
+    
 }
+
+
+
+
+
+
