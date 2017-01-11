6276b626d15f5d2c979253e3098d405dc91e37d4
edmund-h <447ght@gmail.com>
2017-01-11 09:58:25 -0500

    added documentation to code

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -11,13 +11,15 @@ class ShoppingList {
     
     
     func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
-        var fixedShoppingList: [String] = []
-        var index = 0
-        while index < items.count {
-            fixedShoppingList.append("\(index + 1). \(items[index])(\(amounts[index]))" )
-            index += 1
+        var fixedShoppingList: [String] = []    //new list array to return
+        var index = 0                           //index variable for iterating over arrays
+        while index < items.count {             //logic structure to iterate over arrays
+            fixedShoppingList.append("\(index + 1). \(items[index])(\(amounts[index]))" ) //do this, and
+            index+=1                            //increase iterator, for each element in the array
         }
-        return fixedShoppingList
+        return fixedShoppingList                //returns formatted list comprised of input lists
     }
     
 }
+//My first time through I did not understand the enumarated () method, so I implemented my own rudimentary for loop using c logic structures I was already familiar with.
+//In a later lab I used an enumerated array in a two variable for loop to demonstrate that I do understand the concept now.
