6f039eeb946e3b2e4a3e473a561f26487dba9d5f
sandboats <akhil.alt7@gmail.com>
2017-01-27 16:24:09 +0530

    swift-arrayChallenge-lab-ios-apply-000 done

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -9,14 +9,31 @@
 
 class ShoppingList {
     
-    
-    func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String] {
         
        
         // Implement this function
         
+        func createShoppingList(withItems items: [String], amountOfEachItem amounts: [String]) -> [String]{
+            
+            
+            var shoppingList: [String] = []
+            
+            for (index, item) in items.enumerated() {
+                
+                let amount = amounts[index]
+                
+                let shoppingItem = "\(index + 1). \(item)(\(amount))"
+                
+                shoppingList.append(shoppingItem)
+                
+            }
+            
+            return shoppingList
+            
         
+            
+        }
         
-    }
+    
     
 }
