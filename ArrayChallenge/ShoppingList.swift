1377b3d7fe4c9fafb80f70e84f6710676f977b86
“Diljeet <jeetds1994@gmail.com>
2017-04-12 18:17:30 -0400

    created function to solve problems

--- ArrayChallenge/ShoppingList.swift
+++ ArrayChallenge/ShoppingList.swift
@@ -15,8 +15,19 @@ class ShoppingList {
        
         // Implement this function
         
-        
-        
+        print(items)
+        print(amounts)
+        var answer: [String] =  []
+        // insert for func
+        var counter = 0
+        for (index, item) in items.enumerated(){
+            counter += 1
+            answer.append("\(counter). \(item)(\(amounts[index]))")
+                
+        }
+        print(answer)
+        return answer
     }
+        
     
 }
